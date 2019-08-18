d3aeb3cd21641766dc3adf160ba46b922cdf1e2e
dragos <slujerudragos@gmail.com>
2019-08-18 19:07:44 +0300

    default security

---
+++ src/main/java/ro/fortech/security/SpringSecurityApplication.java
@@ -0,0 +1,20 @@
+package ro.fortech.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+}
